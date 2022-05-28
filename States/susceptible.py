c7bf816edd1e44493efcb8093167d4d487daeec6
qLate <uaolegikua@gmail.com>
2022-05-28 15:48:56 +0300

    moved state.py

--- States/susceptible.py
+++ States/susceptible.py
@@ -1,16 +1,21 @@
 import random
 
-from self_isolated import SelfIsolated
 from asymptomatic import Asymptomatic
 from infected import Infected
+from self_isolated import SelfIsolated
 from state import State
 
 
 class Susceptible(State):
+    def __init__(self, human, data):
+        super().__init__(human, data)
+
+        p_a = 0.95 if human.age <= 4 else 0.8 if 5 <= human.age <= 14 else 0.7 if 15 <= human.age <=  else 0.7 if 5 <= human.age <= 14 else 0.7 if 5 <= human.age <= 14 else 0.7
+
     def tick(self):
         super().tick()
 
-        if random.random() < self.human.COEFF1:
+        if random.random() < self.data['q']:
             self.human.setState(SelfIsolated(self.human, self.data))
         elif random.random() < self.human.COEFF1:
             self.human.setState(Asymptomatic(self.human, self.data))
