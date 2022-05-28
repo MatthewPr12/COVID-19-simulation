960e15d7619cadf16f1c54b533bc7c090dbc51cf
Sviat <91615606+sviat-l@users.noreply.github.com>
2022-05-28 18:06:47 +0300

    Immunity coeffs

--- human.py
+++ human.py
@@ -1,48 +1,37 @@
 """Human Class"""
-from numpy import random as np_random
-from random import choice
 import math
+import random
+from random import choice
 
-import scipy.stats
+from States.susceptible import Susceptible
+from normal_distrubution import get_normal_distribution
+
+distribution = get_normal_distribution()
 
 
-N = 10000
-
-
-scipy.stats.truncnorm.rvs((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma,)) * 90
-
-class Sosiety:
-    pass
-class Human(Society):
-
-    def __init__(self):
-        super.__init__()
-        self.current_state = Susceptible(self, Society.data)
-        self.set_State(self.current_state)
+class Human:
+    def __init__(self, data):
+        self.current_state = Susceptible(self, data)
+        self.setState(self.current_state)
         self.immunity_coeff = self.immunity_coefficient()
-        self.curr_state.human = self
-        self.age = self.get_age(self)
-        self.gender = choice(['male','female'])
+        self.current_state.human = self
+        self.age = self.get_normal_distribution_value() * 90
+        self.gender = choice(['male', 'female'])
+
+        self.data = data
 
     def setState(self, state):
-        self.curr_state = state
-        self.curr_state.human = self
+        self.current_state = state
+        self.current_state.human = self
 
     def tick(self):
         self.current_state.tick()
 
-    def get_age():
-        lower = 0
-        upper = 1
-        mu = 0.5
-        sigma = 0.2
-        scipy.stats.truncnorm.rvs((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma,)) * 90
+    def get_normal_distribution_value(self):
+        return random.choice(distribution)
 
-
-    def immunity_coefticient(self):
-            return math.sqrt(1- (self.data['young'] if self.age < 60 else self.data['old']) *\
-                  (self.data['male'] if self.gender == 'male' else self.data['female']))
-
-
-
+    def immunity_coefficient(self):
+        return math.sqrt(1 - (self.data['young'] if self.age < 60 else self.data['old']) *
+                         (self.data['male'] if self.gender == 'male' else self.data[
+                             'female']) * self.get_normal_distribution_value())
 
