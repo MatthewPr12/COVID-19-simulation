e3cbd23275715a8fb492453a380aa749ebfb9590
NaniiiGock <91615473+NaniiiGock@users.noreply.github.com>
2022-05-28 15:52:47 +0300

    Update society.py

--- society.py
+++ society.py
@@ -10,20 +10,23 @@ class Society:
     EMPTY = None
     def __init__(self, num_rows, num_cols, coef_people):
         self.grid = Array2D(num_rows, num_cols)
-        # self.num_of_residents = 
-        self.add_people(coef_people)
-        self.num_of_infeded = 0
+        self.num_of_residents = self.add_people(coef_people)
+        self.num_of_infected = 0
         self.num_of_recovered = 0
         self.num_of_dead = 0
+        self.confirmed = 0
+        self.yesterday_confirmed = 0
     
     def add_people(self, coef):
+        counter = 0
         for i in range(self.grid.num_rows()):
             for j in range(self.grid.num_cols()):
                 if uniform(0,1) < coef:
+                    counter +=1
                     self.grid[i, j] = Society.HUMAN
                 else:
                     self.grid[i, j] = Society.EMPTY
-                    
+        return counter   
 
     def get_neighbors(self, row, col):
         counter = 0
@@ -40,10 +43,29 @@ class Society:
         if self.is_human(row, col):
             counter -= 1
         return counter
+    
+    def count_q(self, yesterday):
+        q = 0.7 - 0.1(self.confirmed - yesterday)/yesterday/0.025
+        return q
+
 
     def is_human(self, row, col):
         return True if self.grid[row, col]== self.HUMAN else False
     
+    def time_flow(self):
+        # for i in range(100):
+          
+            # 1-second cycle
+
+            # counting new 
+
+            # confirmed = count_confirmed()
+            # self.yesterday_confirmed = self.confirmed
+            # self.comfirmed = confimed()
+            # q = self.count_q(self.yesterday_confirmed)
+        pass
+
+
     def __str__(self):
         sstr=''
         for i in range(self.grid.num_rows()):
@@ -51,6 +73,10 @@ class Society:
                 sstr+= "1" if self.is_human(i,j) else"0"
             sstr+="\n" if i != self.grid.num_rows() - 1 else ""
         return sstr
+
+    
+        
+
 
 soc = Society(10, 10, 0.5)
 print(soc)
