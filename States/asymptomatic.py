e9500ffef5d3cfbe9838019806fde88c9ae1b3b8
qLate <uaolegikua@gmail.com>
2022-05-28 15:41:40 +0300

    added few states

---
+++ States/asymptomatic.py
@@ -0,0 +1,10 @@
+from state import State
+from recovered import Recovered
+
+
+class Asymptomatic(State):
+    def tick(self):
+        super().tick()
+
+        if self.time >= self.data["T1"] + self.data["T2"]:
+            self.human.setState(Recovered(self.human, self.data))
