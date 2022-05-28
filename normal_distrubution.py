5a5491493eae00ef3a0ca80c8c5979618ab40164
qLate <uaolegikua@gmail.com>
2022-05-28 17:52:08 +0300

    some more changes

--- normal_distrubution.py
+++ normal_distrubution.py
@@ -7,7 +7,7 @@ def get_normal_distribution():
     mu = 0.5
     sigma = 0.2
     N = 1
-    return [item * 90 for item in list(scipy.stats.truncnorm.rvs(
+    return [item for item in list(scipy.stats.truncnorm.rvs(
         (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma, size=N))]
 
 
