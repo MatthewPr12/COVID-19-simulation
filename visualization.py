5c345035922ea6ac2328ec8cd58f00fca71fbeb0
qLate <uaolegikua@gmail.com>
2022-06-04 19:32:10 +0300

    nice

--- visualization.py
+++ visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 from human import Human
 
 
@@ -15,13 +16,14 @@ def display(array, day_number, ax1, ax2, infected, recovered, dead, inf, rec, de
     ax1.cla()
     ax1.set_title("{}".format('Day ' + str(day_number)))
     ax1.imshow(colors)
-    t=np.arange(0, len(infected), 1)
+    t = np.arange(0, len(infected), 1)
     ax2.plot()
     ax2.plot(t, infected, color='red')
     ax2.plot(t, recovered, color='green')
     ax2.plot(t, dead, color='black')
-    ax2.legend(['infected = '+str(inf), 'recovered = '+str(rec), 'dead = '+str(de)], loc='upper left')
-    ax2.set_xlabel('Day '+str(day_number))
+    ax2.legend(['infected = ' + str(inf), 'recovered = ' + str(rec), 'dead = ' + str(de)],
+               loc='upper left')
+    ax2.set_xlabel('Day ' + str(day_number))
     ax2.set_ylabel('Number of people')
     plt.pause(0.1)
 
