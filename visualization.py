c00f94c11972faa58d4df05e779b1b5ca3e1a62d
Vitalik <91615314+Vitalik001@users.noreply.github.com>
2022-06-04 19:04:30 +0300

    Update visualization.py
    completed visualization

--- visualization.py
+++ visualization.py
@@ -1,36 +1,39 @@
-import numpy as np
 import matplotlib.pyplot as plt
-#add graphics
-class Human:
+import numpy as np
+from human import Human
 
-    def __init__(self, state):
-        self.state = state
-        
-    def get_state(self):
-        return self.state
 
-# self.fig, self.ax=plt.subplots()
-fig, ax = plt.subplots()
-
-def show_day(array, day_number):
+def display(array, day_number, ax1, ax2, infected, recovered, dead, inf, rec, de):
     GREY = (0.78, 0.78, 0.78)  # uninfected
     RED = (0.96, 0.15, 0.15)  # infected
     GREEN = (0, 0.86, 0.03)  # recovered
-    BLACK = (0, 0, 0) #dead
-    WHITE=(1., 1., 1.) #empty position
-    colors=np.asarray([[get_human_color(human) if isinstance(human, Human) else WHITE for human in arr] for arr in array])
-    ax.cla()
-    ax.imshow(colors)
-    ax.set_title("{}".format('Day '+str(day_number))) #add additional info
-    plt.pause(3)
+    BLACK = (0, 0, 0)  # dead
+    WHITE = (1., 1., 1.)  # empty position
+    colors = np.asarray(
+        [[get_human_color(human) if isinstance(human, Human) else WHITE for human in arr] for arr in
+         array])
+    ax1.cla()
+    ax1.set_title("{}".format('Day ' + str(day_number)))
+    ax1.imshow(colors)
+    t=np.arange(0, len(infected), 1)
+    ax2.plot()
+    ax2.plot(t, infected, color='red')
+    ax2.plot(t, recovered, color='green')
+    ax2.plot(t, dead, color='black')
+    ax2.legend(['infected = '+str(inf), 'recovered = '+str(rec), 'dead = '+str(de)], loc='upper left')
+    ax2.set_xlabel('Day '+str(day_number))
+    ax2.set_ylabel('Number of people')
+    plt.pause(0.1)
+
 
 def get_human_color(human):
-    states={'uninfected': (0.78, 0.78, 0.78), 'empty_pos': (1, 1, 1), 'self_isolated': (0.78, 0.78, 0.78),'infected': (0.96, 0.15, 0.15), 'confirmed':(0.96, 0.15, 0.15), 'recovered': (0, 0.86, 0.03), 'hospitalized': (0.96, 0.15, 0.15), 'dead': (0, 0, 0), 'asymptomatic': (0.96, 0.15, 0.15)}
-    return states[human.get_state()]
-
-
-if __name__=='__main__':
-    grid=np.array([[None, Human('uninfected')], [Human('empty_pos'), Human('infected')]])
-    show_day(grid, 1)
-    grid=np.array([[None, Human('recovered')], [Human('confirmed'), Human('self_isolated')]])
-    show_day(grid, 2)
+    GREY = (0.78, 0.78, 0.78)  # uninfected
+    RED = (0.96, 0.15, 0.15)  # infected
+    GREEN = (0, 0.86, 0.03)  # recovered
+    BLACK = (0, 0, 0)  # dead
+    WHITE = (1., 1., 1.)  # empty position
+    states = {'Susceptible': GREY, 'SelfIsolated': (0.2, 0.78, 0.78),
+              'Infected': RED, 'Confirmed': (0.96, 0.45, 0.45),
+              'Recovered': GREEN, 'Hospitalized': (0.96, 0.8, 0.15), 'Dead': BLACK,
+              'Asymptomatic': (0.96, 0.15, 0.15)}
+    return states[str(human.getState().__class__.__name__)]
